84fdecb4cbabdd58a15e694ab78a3c5496ef6fef
taciano <tacianohora2020@hotmail.com>
2023-01-26 21:29:13 -0300

    feat(src/index.js): rota para criar usuário

--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
+function checksExistsUserAccount(req, res, next) {
   // Complete aqui
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', (req, res) => {
+  const { name, username } = req.body;
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  };
+
+  users.push(user);
+  return res.status(201).send();
+});
+
+app.get('/todos', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.post('/todos', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.put('/todos/:id', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.patch('/todos/:id/done', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete('/todos/:id', checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
